1f85011f19c4ee1a0a59601bd2be462c3a50dd40
Aji Lantang Mardika <email": null>
2016-11-30 12:15:37 +0700

    create register

---
+++ personalNotesRelease1/index.ios.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Register from './app/containers/Register'
+
+export default class personalNotesRelease1 extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Register/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('personalNotesRelease1', () => personalNotesRelease1);
